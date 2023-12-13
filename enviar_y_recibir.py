06c68e263ee1139288daf28c6d4ad76e9042bc41
JhonatanM22 <152195444+JhonatanM22@users.noreply.github.com>
2023-12-13 00:14:12 -0500

    enviar_y_recibir.py

--- enviar_y_recibir.py
+++ enviar_y_recibir.py
@@ -1,25 +1,26 @@
 import paho.mqtt.client as mqtt
 import psutil
 import platform
-import datetime
 import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from datetime import datetime
 import mysql.connector
+from mysql.connector import Error
+
+# Configuración de la base de datos MySQL
+db_host = "localhost"
+db_user = "root"
+db_password = "K4m1s4T0.Ayak4"
+db_name = "Arquitectura"
 
 # Configuración del broker MQTT
 broker_address = "broker.hivemq.com"
 port = 1883
-topic = "mateo"
+topic = "metadatos"
 #Topico al que se enviará la información
-topic_diferencia_metadatos = "metadatos"
-
-# Configuración de la base de datos MySQL
-db_host = "tu_host"
-db_user = "tu_usuario"
-db_password = "tu_contraseña"
-db_name = "nombre_de_tu_base_de_datos"
+topic_diferencia_metadatos = "topico2"
 
 # Configuración del servidor SMTP para enviar correos
 smtp_server = "smtp.gmail.com"
@@ -28,8 +29,11 @@ smtp_username = "mmales385@gmail.com"
 smtp_password = "acir fpif edes uzsq"
 recipient_email = "mateomales2003@gmail.com"
 
-# Función para conectar a la base de datos
-def conectar_base_de_datos():
+# Identificador del equipo (remitente)
+remitente = "Mateo"
+
+# Función para conectar a la base de datos MySQL
+def conectar_mysql():
     try:
         connection = mysql.connector.connect(
             host=db_host,
@@ -37,14 +41,23 @@ def conectar_base_de_datos():
             password=db_password,
             database=db_name
         )
-        return connection
-    except mysql.connector.Error as err:
-        print(f"Error al conectar a la base de datos: {err}")
+        if connection.is_connected():
+            print(f"Conectado a la base de datos: {db_name}")
+            return connection
+    except Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
         return None
 
-# Función para obtener la fecha actual
-def obtener_fecha_actual():
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# Función para insertar datos en la tabla 'data'
+def insertar_en_tabla_data(connection, info, percent):
+    try:
+        cursor = connection.cursor()
+        sql_query = "INSERT INTO data (info, percent) VALUES (%s, %s)"
+        cursor.execute(sql_query, (info, percent))
+        connection.commit()
+        print("Datos insertados en la tabla 'data'.")
+    except Error as e:
+        print(f"Error al insertar datos en la tabla 'data': {e}")
 
 # Función que se ejecuta cuando se conecta al broker
 def on_connect(client, userdata, flags, rc):
@@ -53,10 +66,9 @@ def on_connect(client, userdata, flags, rc):
 
 # Función que se ejecuta cuando se recibe un mensaje
 def on_message(client, userdata, msg):
-    fecha_actual = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    mensaje_completo = f"\nMensaje recibido el {fecha_actual}:\n {msg.payload.decode()}"
-    print(mensaje_completo)
-
+    fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    remitente_mensaje = msg.topic.split("/")[-1]  # Extrae el remitente del tópico
+    print("\nMensaje recibido en {} de {}:\n {}".format(fecha_actual, remitente_mensaje, msg.payload.decode()))
 
 # Función para obtener el rendimiento del CPU
 def obtener_rendimiento_cpu():
@@ -158,11 +170,10 @@ try:
         rendimiento_memoria = obtener_rendimiento_memoria()
         rendimiento_red = obtener_rendimiento_red()
         sistema_operativo = obtener_sistema_operativo()
-        fecha_actual = obtener_fecha_actual()  # Agrega la fecha actual
 
         # Formatea los datos como un mensaje
         mensaje = (
-            f"Fecha y Hora del mensaje: {fecha_actual}\n"
+            f"PC de: {remitente}\n"
             f"Rendimiento del CPU (%): {rendimiento_cpu}\n"
             f"Rendimiento de la Memoria (%): {rendimiento_memoria}\n"
             f"Rendimiento de la Red (GB): {rendimiento_red}\n"
@@ -172,18 +183,30 @@ try:
         # Envia el mensaje al broker MQTT
         client.publish(topic, mensaje)
 
-        # Limpia el búfer de la consola
-        os.system('cls' if os.name == 'nt' else 'clear')
+         # Inserta los datos en la tabla 'data' de la base de datos MySQL
+        connection = conectar_mysql()
+        if connection:
+            insertar_en_tabla_data(connection, "PC de", remitente)
+            insertar_en_tabla_data(connection, "Rendimiento del CPU", rendimiento_cpu)
+            insertar_en_tabla_data(connection, "Rendimiento de la Memoria", rendimiento_memoria)
+            insertar_en_tabla_data(connection, "Rendimiento de la Red", rendimiento_red)
+            insertar_en_tabla_data(connection, "Sistema Operativo", sistema_operativo)
+            connection.close()
 
-        # Imprime el mensaje para el usuario
-        print("Mensaje enviado:\n", mensaje)
+         # Obtiene la fecha y hora actual
+        fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # Limpia el búfer de la consola
+        os.system("cls" if os.name == "nt" else "clear")
+
+        # Imprime la fecha y el mensaje para el usuario
+        print("Mensaje enviado en {}:\n {}".format(fecha_actual, mensaje))
 
         # Verifica y envía una alerta si es necesario
         verificar_y_enviar_alerta(mensaje)
 
         # Espera a que el usuario presione ENTER para enviar el siguiente mensaje
         input("\nPresiona ENTER para enviar el siguiente mensaje...\n")
-
 
 except KeyboardInterrupt:
     # Desconectar al recibir una interrupción del teclado (Ctrl+C)
